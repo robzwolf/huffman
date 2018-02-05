19b1962d5600e4762528ec985e5bd39142169eca
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2018-02-05 14:30:38 +0000

    Started implementing Tree class. Neatened up huffman.py

--- huffman.py
+++ huffman.py
@@ -1,28 +1,51 @@
-# Read the file and count frequencies of characters
 from collections import defaultdict
+from time import time
+import sys
+from tree import Tree
+
+# Start time
+t0 = time()
 
 # Initialise the frequencies dictionary
 freqs = defaultdict(int)
 
-filename = "lorem_ipsum.txt"
-# filename = "chinese_poem.txt"
-# filename = "emoji.txt"
+# Get the filename to read from the arguments
+if len(sys.argv) < 2:
+    print("No filename was specified.")
+    sys.exit(0)
+else:
+    filename = sys.argv[1]
+
+
+# Read the file and count frequencies of characters
+running_total = 0
+
+# Loop through every character of the file
 with open(filename) as f:
     while True:
+        # Read a single character
         c = f.read(1)
+
         if not c:
-            # print("End of file")
+            # If we've reached the end of the file
             break
+
         else:
             if c not in freqs:
+                # Set the first occurrence of c to frequency 1
                 freqs[c] = 1
-            else:
-                freqs[c] += 1
-            # print("freqs[" + c + "] =", freqs[c])
-        # print("Read a character:", c)
 
-print("freqs dictionary has the following frequencies:")
-for char in freqs:
-    print(char, ":", freqs[char])
+            else:
+                # Increment the number of occurrences of c by 1
+                freqs[c] += 1
+
+        # Keep a track of how many characters were in the file
+        running_total += 1
+
+# End time
+t1 = time()
 
 print(freqs)
+print("Total characters:", "{:,}".format(running_total))
+print("Process took " + str(round(t1-t0,5)) + " seconds")
+
